554de7545374d2b2439a69132421b6550bfd07cc
PAVANAKUMARA <pavanhuli481@gmail.com>
2023-03-19 21:45:07 -0700

    Create Test21.java
    done

---
+++ Test21.java
@@ -0,0 +1,18 @@
+//reading file
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+public class Test21 {
+	
+	public static void main(String[] args) throws Exception {
+		File fileob=new File("D:\\huli\\multithreading.txt");
+		FileReader fr = new FileReader(fileob);
+		BufferedReader br = new BufferedReader(fr);
+		String line;
+		while((line=br.readLine())!=null) {
+			System.out.println(line);
+		}
+		br.close();
+        }
+       	}
+
