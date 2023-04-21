8430dab5ad14285d56b5318c8f3989bd39edf943
PAVANAKUMARA <pavanhuli481@gmail.com>
2023-04-21 13:37:42 +0530

    jdbc
     done

---
+++ Test32.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+public class Test32 {
+	public static void main(String[] args) throws Exception {
+		//step1 loading the driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//step2 getting the connection
+		String address="jdbc:mysql://localhost:3306/4cse2userdb";
+		Connection connection= DriverManager.getConnection(address,"root","root");
+		//step3 getting the prompt
+		Statement st=connection.createStatement();
+		String insertquery="insert into user values('0032','tiger','pavan','huli@gmail.com')";
+		//step4 passing the query to prompt
+		int n=st.executeUpdate(insertquery);
+		//step5 get the respons
+         System.out.println(n +" inserted");
+	}
+}
